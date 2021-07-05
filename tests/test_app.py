72d139badda0d8ef8e341f554e45bb709e7305b6
BalbinoEsen <balbino.aylagas@esen.edu.sv>
2021-07-04 21:45:53 -0600

    added a new test for home

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,10 @@
-from app import home
+from flask import Flask
+from routes.main_routes import MainRoutes
 
 
 def test_index():
-    assert home() == "this is my main site"
+    app = Flask(__name__, template_folder="../src/templates/")
+    MainRoutes.configure_routes(app)
+    with app.test_client() as client:
+        result = client.get("/")
+        assert "this is my main" in str(result.data)
